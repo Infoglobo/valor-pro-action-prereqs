2e2fbf242052d6d52a9681de0ed7cafdf68a2520
Mauricio Gamarra <mauricio.gamarra.gbi@edglobo.com.br>
2023-03-07 01:57:57 -0300

    first version

--- script.sh
+++ script.sh
@@ -27,22 +27,44 @@ echo "$FIRST_LAUNCHSETTINGS"
 
 SUCCESS=true
 if [ ! -z "$FIRST_LAUNCHSETTINGS" ]; then
-    ENV_PROPS=$(env)
+    ENV_PROPS=
+
+ 
 
     AMBIENTE=${GITHUB_REF_NAME##*/}
     AMBIENTE=${AMBIENTE,,}
-    #garantir que a pasta seja sempre enviroments/main
-    if [ "$GITHUB_REF_NAME" == "master" ] || [ "$GITHUB_REF_NAME" == "main"  ] ; then
+ 
+
+    SECRETS_PREFIX=${AMBIENTE^^}
+
+    if [ "$GITHUB_REF_NAME" = "dev" ] ; then
+        SECRETS_PREFIX="DEV"
+    elif [ "$GITHUB_REF_NAME" = "homolog" ] ; then
+        SECRETS_PREFIX="HML"
+    elif [ "$GITHUB_REF_NAME" == "master" ] || [ "$GITHUB_REF_NAME" == "main"  ] ; then
+        SECRETS_PREFIX="PRD"
+        #garantir que a pasta seja sempre enviroments/main
         AMBIENTE="main"
-    fi 
+    fi  
+    SECRETS_PREFIX=${SECRETS_PREFIX^^}
+
+
+    env | grep ^"$SECRETS_PREFIX" | 
+    while IFS='=' read -r key value; do
+        key="${key/#${SECRETS_PREFIX}_/}"
+        echo "ENV_PROPS carregando $key das secrets do github" 
+        ENV_PROPS="${ENV_PROPS}\n$key=$value"
+    done 
+
     FOLDER_REPO_NAME=$(pwd)
     PROPERTY_FILE="$FOLDER_REPO_NAME/enviroments/$AMBIENTE/cm.properties"
     cat $PROPERTY_FILE
     # verifica se o arquivo existe e não está vazio e carrega as propriedades
     if  [ -s "$PROPERTY_FILE" ]; then
         echo "carregando as propriedades do arquivo $PROPERTY_FILE"
-        while IFS='=' read -r k v; do
-            ENV_PROPS="${ENV_PROPS}\n$k=$v"
+        while IFS='=' read -r key value; do
+            echo "ENV_PROPS carregando $key do arquivo  $PROPERTY_FILE" 
+            ENV_PROPS="${ENV_PROPS}\n$key=$value"
         done < $PROPERTY_FILE
     fi
 
