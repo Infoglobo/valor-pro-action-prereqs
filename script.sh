ac2316abae0461b7577fa045a503f4330a73bd27
Mauricio Gamarra <mauricio.gamarra.gbi@edglobo.com.br>
2023-03-07 12:43:16 -0300

    teste com pipefail

--- script.sh
+++ script.sh
@@ -58,14 +58,24 @@ if [ ! -z "$FIRST_LAUNCHSETTINGS" ]; then
     SECRETS_PREFIX=${SECRETS_PREFIX^^}
 
     ENV_PROPS=""
-    env | grep ^"$SECRETS_PREFIX" | 
+    #env | grep ^"$SECRETS_PREFIX" | 
+    #while IFS='=' read -r key value; do
+#
+    #    key="${key/#${SECRETS_PREFIX}_/}"
+    #    echo "ENV_PROPS carregando das secrets do github $key " 
+    #    ENV_PROPS="${ENV_PROPS}\n$key=$key"
+    #    echo "$ENV_PROPS"
+    #    
+    #done 
+
+    env | grep ^"$SECRETS_PREFIX" > temp.txt
+    cat temp.txt
+    
     while IFS='=' read -r key value; do
-        key="${key/#${SECRETS_PREFIX}_/}"
-        echo "ENV_PROPS carregando das secrets do github $key " 
-        ENV_PROPS="${ENV_PROPS}\n$key=$key"
-        echo "$ENV_PROPS"
-        
-    done 
+        echo "ENV_PROPS carregando do arquivo $key $PROPERTY_FILE" 
+        ENV_PROPS="${ENV_PROPS}\n$key=$value"
+    done < temp.txt    
+    
     echo "SHOW ENV_PROPS"
     echo "$ENV_PROPS"
     ENV_PROPS="${ENV_PROPS}\n**FIM SECRETS**"
